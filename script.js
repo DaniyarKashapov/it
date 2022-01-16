b4ab96b102f009500717795b7eca06a75420fc5c
DaniyarKashapov <90697327+DaniyarKashapov@users.noreply.github.com>
2022-01-16 14:16:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const herob = document.getElementById('herob'),
+      cs = document.getElementById('cs'),
+      ps = document.getElementById('Play');
+
+cs.addEventListener('click', () => {
+    alert('На данный момент, данный сервис недоступен!');
+});
+
+herob.addEventListener('click', () => {
+    alert('На данный момент, данный сервис недоступен!');
+});
+
+Play.addEventListener('click', () => {
+    alert('На данный момент, данный сервис недоступен!');
+});
+
